a3c742b82a545d3d72d8cac85c298eed032b1d5e
Alex <alexpak1988@gmail.com>
2023-07-11 18:18:10 +0500

    corrected
    Took 7 minutes

--- API/test_process.py
+++ API/test_process.py
@@ -8,11 +8,12 @@ test_bp = Blueprint('test_process', __name__)
 #Валидатор вопросов
 class Questions(BaseModel):
     timer: int
-    questions: List[Dict[int,Dict[str, List]]]
+    # questions: List[Dict[int,Dict[str, List]]]
+    questions: List[Dict]
 
 #Запрос на получение всех вопросов
 @test_bp.route('/get-question/<string:level>', methods=['GET'])
-def get_user_questions(level:str)-> Questions:
+def get_user_questions(level:str)-> Questions.json:
     #Обращение к базе вопросов
     questions = get_questions_db(level)
     if questions:
@@ -20,17 +21,18 @@ def get_user_questions(level:str)-> Questions:
         timer = 0
         #пройдемся по каждому вопросу
         for question in questions:
-            generated_question = {question.main_question:[question.answer_1,
-                                                          question.answer_2,
-                                                          question.answer_3,
-                                                          question.answer_4]}
-            result.append({question.id:generated_question})
+            generated_question = {'question_text': question.main_question,
+                                  'variants': [question.answer_1,
+                                               question.answer_2,
+                                               question.answer_3,
+                                               question.answer_4]}
+            result.append({'question_id': question.id, 'question': generated_question})
             timer=question.timer
-        return Questions(**{'timer':timer, 'questions':result})
-    return Questions(timer=0, questions=[])
+        return Questions(**{'timer': timer, 'questions': result}).json()
+    return Questions(timer=0, questions=[]).json()
 
 #Запрос на проверку ответа от пользователя
-@test_bp.route('/check-answer/<int:question_id>/<string:user_answer>', methods=['POST'])
+@test_bp.route('/check-answer/<int:question_id>/<int:user_answer>', methods=['POST'])
 def check_current_user_answer(question_id:int, user_answer:str)-> Dict[str, int]:
     result = check_user_answer_db(question_id, user_answer)
     return {'status':1 if result else 0}
